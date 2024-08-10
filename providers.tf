4d740a3f8949d4ef5e45bb548b26962446e2d513
Demo-8652 <134642574+Demo-8652@users.noreply.github.com>
2024-08-10 16:09:37 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -17,6 +17,6 @@ provider "azurerm" {
 
 # Import the Azure Resource Group
 resource "azurerm_resource_group" "k8s" {
-  name     = "664-8d8a803e-create-an-aks-cluster-in-azure-with-t"
-  location = "eastus"
+  name     = "664-394a23a1-create-an-aks-cluster-in-azure-with-t"
+  location = "South Central US"
 }
