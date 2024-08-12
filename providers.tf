6067c37b8e88772121c724d2098ed9fb1458a41c
Demo-8652 <134642574+Demo-8652@users.noreply.github.com>
2024-08-13 02:33:01 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -17,6 +17,6 @@ provider "azurerm" {
 
 # Import the Azure Resource Group
 resource "azurerm_resource_group" "k8s" {
-  name     = var.resourceGroups
-  location = var.Location
+  name     = var.RESOURCEGROUPS
+  location = var.LOCATION
 }
