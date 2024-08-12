50c4ddd6cb866f69bf81db02a5b16fb69dd2e6e9
Demo-8652 <134642574+Demo-8652@users.noreply.github.com>
2024-08-13 04:18:14 +0530

    Update aks.tf

--- aks.tf
+++ aks.tf
@@ -19,8 +19,8 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   }
 
   service_principal {
-    client_id     = var.PRINCIPLEID
-    client_secret = var.CLIENT
+    client_id     = var.principleID
+    client_secret = var.client
   }
 
   network_profile {
